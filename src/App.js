759720b69a29877873f4d497ec1af778ea4421c9
Lucky <rlucky469@gmail.com>
2024-09-02 19:25:57 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import AdminPanel from "./pages/AdminPanel";
+import UserInfo from "./pages/UserInfo";
+import Users from "./pages/Users";
+import Login from "./pages/Login";
+import { UserProvider } from "./contexts/UserContext";
+import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <UserProvider>
+        {/* wrap the application in the UserProvider to provide user context */}
+        <Header />
+        <Routes>
+          {/* Public route for login  */}
+          <Route path="/" element={<Login />} />
+
+          {/* Private route for dashboard accessible to ADMIN and user Roles */}
+          <Route
+            path="dashboard"
+            element={
+              <PrivateRoute allowedRoles={["ADMIN", "USER"]}>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          {/* Private route for admin panel accessible to ADMIN role only */}
+          <Route
+            path="admin"
+            element={
+              <PrivateRoute allowedRoles={["ADMIN"]}>
+                <AdminPanel />
+              </PrivateRoute>
+            }
+          />
+
+          {/* Private route for users accessible to ADMIN role only */}
+          <Route
+            path="users"
+            element={
+              <PrivateRoute allowedRoles={["ADMIN"]}>
+                <Users />
+              </PrivateRoute>
+            }
+          />
+
+          {/*this route accessible to USER role only */}
+
+          <Route
+            path="user-info"
+            element={
+              <PrivateRoute allowedRoles={["USER"]}>
+                <UserInfo />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </UserProvider>
+    </>
   );
 }
 
